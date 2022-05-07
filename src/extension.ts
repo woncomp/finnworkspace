65d02108ad7742dc0cb346b9e21aabd16d1b6e89
Joshua Zhang <woncomp@gmail.com>
2022-05-07 14:23:05 +0800

    List and open recent workspaces

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,44 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+async function quickSwitch() {
+	let items: vscode.QuickPickItem[] = [];
+
+	const recentlyOpened: any = await vscode.commands.executeCommand('_workbench.getRecentlyOpened');
+	let index: number = 0;
+	for(let recentItem of recentlyOpened.workspaces) {
+		if('workspace' in recentItem) {
+			index += 1;
+			items.push( {
+				label: "[Recent " + index + "]",
+				description: recentItem.workspace.configPath.fsPath,
+			});
+		}
+	}
+
+	let options: vscode.QuickPickOptions = {
+		placeHolder: "Select the Workspace you want to switch to:",
+		canPickMany: false,
+		ignoreFocusOut: true,
+		matchOnDescription: true,
+		matchOnDetail: true
+	};
+
+	let pickedItem = await vscode.window.showQuickPick(items, options);
+	let path = pickedItem?.description ?? null;
+
+	if(path){
+		let workspaceUri = vscode.Uri.file(path);
+		// Open the Workspace
+		await vscode.commands.executeCommand('vscode.openFolder', workspaceUri);
+	}
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "finnworkspace" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('finnworkspace.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Finn&#39;s Workspace List!');
+	let disposable = vscode.commands.registerCommand('finnworkspace.quickswitch', () => {
+		quickSwitch();
 	});
 
 	context.subscriptions.push(disposable);
